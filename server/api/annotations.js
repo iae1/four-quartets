fb8dd4d7404bd84a5182b79746f063017dbf9f07
Isaac Easton <iae1@protonmail.com>
2021-06-26 17:31:18 -0400

    Builds get route for all annotations on backend

--- server/api/annotations.js
+++ server/api/annotations.js
@@ -4,6 +4,21 @@ const { requireToken, isAdmin, annotRequireToken } = require("./gateKeeping");
 const { models: { User, Poem, Line, Annotation } } = require("../db");
 module.exports = router;
 
+// GET all annotations for a given poem
+
+router.get('/:poemId', async (req, res, next) => {
+    try {
+        const allPoemAnnotations = await Annotation.findAll({
+            where: {
+                poem: req.params.poemId
+            }
+        })
+        res.json(allPoemAnnotations)
+    } catch (error) {
+        next(error)
+    }
+})
+
 //DELETE annotation /api/annotations/:id
 // router.delete('/:id', async (req, res, next) => {
 //   try {
