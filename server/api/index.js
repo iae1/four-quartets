13bbbb3fd9c736f33225764f53bcdb995d797854
Isaac Easton <iae1@protonmail.com>
2021-06-26 17:31:49 -0400

    Adds annotation route to api index file

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router;
 router.use("/users", require("./users"));
 router.use("/lines", require("./lines"));
 router.use("/poems", require("./poems"));
-// router.use('/annotations', require('./annotations'))
+router.use('/annotations', require('./annotations'))
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
