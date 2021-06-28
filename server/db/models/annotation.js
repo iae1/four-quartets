ba1176ed72b4293e4cae53ad66250b17b7569329
Isaac Easton <iae1@protonmail.com>
2021-06-28 17:46:01 -0400

    remove idx column from annotation model

--- server/db/models/annotation.js
+++ server/db/models/annotation.js
@@ -2,12 +2,6 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Annotation = db.define("annotation", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
   poem: {
     type: Sequelize.STRING,
     allowNull: false
@@ -23,12 +17,6 @@ const Annotation = db.define("annotation", {
       min: 5
     }
   },
-  charStart: {
-    type: Sequelize.INTEGER
-  },
-  charEnd: {
-    type: Sequelize.INTEGER
-  }
 });
 
 module.exports = Annotation;
