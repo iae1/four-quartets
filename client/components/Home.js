e042c3f4286d254758b422269e5fcec1cae0f829
Isaac Easton <iae1@protonmail.com>
2021-06-29 07:46:06 -0400

    Adds images to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const Home = props => {
   return (
-    // <div>
-    //   <h1>Welcome to Four Quartets Genius App</h1>
-    //   <p>
-    //     Please enjoy commenting on T.S. Eliot's masterpiece
-    //   </p>
-    // </div>
+    <>
+    <div>
+      <h1>Four Quartets Genius App</h1>
+      <p>
+        Please enjoy commenting on T.S. Eliot's masterpiece
+      </p>
+    </div>
     <header className="header">
       {/* <div class="logo-box">
         <img src="img/Cat_Logo_3.png" alt="Logo" className="logo" />
@@ -16,10 +17,15 @@ const Home = props => {
       <div className="text-box">
         <h1 className="heading-primary">
           <span className="heading-primary-main">Four Quartets</span>
-          <span className="heading-primary-sub">in my beginning is my end</span>
+          <span className="heading-primary-sub">In my beginning is my end</span>
         </h1>
       </div>
     </header>
+    <img className="opening-image" src="./assets/burnt-norton.jpg" />
+    <img className="opening-image" src="./assets/east-coker.jpg" />
+    <img className="opening-image" src="./assets/dry-salvages.jpg" />
+    <img className="opening-image"src="./assets/little-gidding.jpg" />
+    </>
   );
 };
 
