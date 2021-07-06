8faf173ee612a1c2792d15196b81234f5299f793
Isaac Easton <iae1@protonmail.com>
2021-07-05 21:40:49 -0400

    brings back poem file after deleted

---
+++ server/api/poems.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const Genius = require("genius-lyrics");
+const Client = new Genius.Client(); // Scrapes if no key is provided
+
+const { models: { Poem, Annotation, Line } } = require("../db");
+const {
+  requireToken,
+  isAdmin,
+} = require("./gateKeeping");
+module.exports = router;
+
+// GET single poem from the rap genius api /api/poems/:id
+router.get("/:id", async (req, res, next) => {
+  
+  try {
+    const title = req.params.id
+      .split("-")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    const searches = await Client.songs.search(`Four Quartets: ${title}`);
+    const firstSong = searches[0];
+    const lyrics = await firstSong.lyrics();
+
+    res.json(lyrics)
+  } catch (error) {
+    next(error)
+  }
+})
